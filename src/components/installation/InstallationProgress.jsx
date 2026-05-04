1370f4fcc5bee26ae852c51b5292d0ed85317d6a
Max Haack <max@caligra.com>
2026-05-04 15:16:51 -0400

    fix: lint errors

--- src/components/installation/InstallationProgress.jsx
+++ src/components/installation/InstallationProgress.jsx
@@ -16,7 +16,6 @@ import { PendingIcon } from "@patternfly/react-icons/dist/esm/icons/pending-icon
 
 import { BossClient, getSteps, installWithTasks } from "../../apis/boss.js";
 
-
 import { OsReleaseContext } from "../../contexts/Common.jsx";
 
 import { EmptyStatePanel } from "cockpit-components-empty-state.jsx";
